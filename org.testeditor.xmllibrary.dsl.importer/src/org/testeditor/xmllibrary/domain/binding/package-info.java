17874401274a3d0f5287b4b7455c35c05ddf4ee2
Franz Becker <franz.becker@itemis.de>
2015-06-17 13:49:15 +0200

    [TE-188] delete old prototype

--- org.testeditor.xmllibrary.dsl.importer/src/org/testeditor/xmllibrary/domain/binding/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package was copied from the test-editor plugin:
- * <p>{@code org.testeditor.xmllibrary/srcGenerated}</p>
- * for prototyping.
- */
-package org.testeditor.xmllibrary.domain.binding;
