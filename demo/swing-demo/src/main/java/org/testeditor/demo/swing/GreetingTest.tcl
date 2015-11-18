c0f94da44860574f56505b22a89d99f4744aa023
Franz Becker <franz.becker@itemis.de>
2015-11-18 19:11:07 +0100

    [TE-291] fix demo project

--- demo/swing-demo/src/main/java/org/testeditor/demo/swing/GreetingTest.tcl
+++ demo/swing-demo/src/main/java/org/testeditor/demo/swing/GreetingTest.tcl
@@ -18,13 +18,16 @@ import org.testeditor.demo.swing.*
 * Start the famous greetings application.
  
     Mask: GreetingApplication
-    - Starte Anwendung "org.testeditor.swing.exammple.Greetings"
+    - Starte Anwendung "org.testeditor.demo.swing.GreetingApplication"
  
-* send greetings "Hello World" to the world.
+* Send greetings "Hello World" to the world.
  
     Mask: GreetingApplication
-    - gebe in "Eingabefeld" den Wert "Hello World" ein
-    - klicke auf Greetings
+    - Gebe "Hello World" in das Feld <Input> ein 
+    - Klicke auf <GreetButton>
+    - Warte "2000" ms   
  
+* Stop the famous greeting application.
+
 	Mask: GreetingApplication
-	- mach was "schlaues"
+	- Stoppe Anwendung
