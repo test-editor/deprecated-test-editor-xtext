548bc66cd00878d02bf76042e27be92c406f408a
Karsten Panier <kpanier@aysada.org>
2015-11-20 11:36:52 +0100

    TE-291 Play the spec for ui and service level

--- demo/swing-demo/src/main/java/org/testeditor/demo/swing/GreetingTestService.tcl
+++ demo/swing-demo/src/main/java/org/testeditor/demo/swing/GreetingTestService.tcl
@@ -4,12 +4,14 @@ package org.testeditor.demo.swing
 
 * Start the famous greetings application
 Mask: GreetingApplication
-
+ - Starte Anwendung "org.testeditor.demo.swing.GreetingApplication"
 
 * Send greetings "Hello World" to the world
 Mask: helloService
+- Rufe Service "name"
 
 
 * Stop the famous greeting application
-
+Mask: GreetingApplication
+ - Stoppe Anwendung
 
