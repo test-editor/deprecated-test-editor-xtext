6419f3b84fe82dfa382a006946b8b3d92aafbe28
Karsten Panier <kpanier@aysada.org>
2016-08-21 14:28:21 +0200

    TE-413 Set package declaration from file structure
    - Fix build

--- rcp/org.testeditor.rcp4.uatests/src/test/java/org/testeditor/rcp4/writetests/WriteSimpleTestWithoutPackageTest.tcl
+++ rcp/org.testeditor.rcp4.uatests/src/test/java/org/testeditor/rcp4/writetests/WriteSimpleTestWithoutPackageTest.tcl
@@ -1,4 +1,3 @@
-
 import org.testeditor.rcp4.*
 
 # WriteSimpleTestWithoutPackageTest
@@ -8,14 +7,14 @@ config TestEditorConfig
 
 * Given Webproject
 
-	Macro: TeRcp4
-	- Create Web example project
+//	Macro: TeRcp4
+//	- Create Web example project
 
-	Component: ProjectExplorer
-	- Select element "MyFirstProject/Tests/MyFirstProject" in tree <ProjektBaum>
-	- Execute menu item "New/Test Case" in tree <ProjektBaum>
-
-	Component: NewDialog
-	- Click on <FinishButton>
+//	Component: ProjectExplorer
+//	- Select element "MyFirstProject/Tests/MyFirstProject" in tree <ProjektBaum>
+//	- Execute menu item "New/Test Case" in tree <ProjektBaum>
+//
+//	Component: NewDialog
+//	- Click on <FinishButton>
 	
 //TODO continue this test after support for text editor is merged	
