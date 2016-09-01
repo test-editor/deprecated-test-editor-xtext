965bba9feb5d968504a293db38d113d6c443b82e
Franz Becker <franz.becker@itemis.de>
2016-09-01 18:05:49 +0200

    TE-411 add test case for renaming without an open editor

--- rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/eclipse/EclipseWorkbenchFixture.java
+++ rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/eclipse/EclipseWorkbenchFixture.java
@@ -1,21 +1,29 @@
 package org.testeditor.fixture.eclipse;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IPathEditorInput;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.testeditor.fixture.core.interaction.FixtureMethod;
 
@@ -36,7 +44,7 @@ public class EclipseWorkbenchFixture {
 	@FixtureMethod
 	public IEditorPart getEditorWithFilePath(String filepath) {
 		logger.info("Searching for open editor with filepath='{}'", filepath);
-		IPath path = getAbsolutelPath(filepath);
+		IPath path = getAbsolutePath(filepath);
 		Stream<IEditorReference> editorReferences = getEditorReferences();
 		IEditorReference editorReference = editorReferences.filter(editorRef -> hasFileAsInput(editorRef, path)).findFirst().orElseThrow(() -> {
 			logger.error("No open editor with filepath='{}' was found.", filepath);
@@ -47,11 +55,35 @@ public class EclipseWorkbenchFixture {
 
 	@FixtureMethod
 	public String getEditorContents(IEditorPart editorPart) {
+		logger.info("Getting contents for editor with input='{}'.", editorPart.getEditorInput());
 		if (editorPart instanceof ITextEditor) {
 			IDocument document = ((ITextEditor) editorPart).getDocumentProvider().getDocument(editorPart.getEditorInput());
 			return document.get();
 		}
 		throw new IllegalArgumentException("The passed editor was not a text editor!");
+	}
+
+	@FixtureMethod
+	public IEditorPart openEditor(String filepath) {
+		logger.info("Opening editor for filepath='{}'.", filepath);
+		IFile file = getFile(filepath);
+		IWorkbenchPage page = getActivePageOrFirst();
+		return syncExec(() -> IDE.openEditor(page, file));
+	}
+
+	@FixtureMethod
+	public void closeEditor(IEditorPart editorPart) {
+		logger.info("Closing editor with input='{}'.", editorPart.getEditorInput());
+		IWorkbenchWindow window = editorPart.getSite().getWorkbenchWindow();
+		syncExec(() -> window.getActivePage().closeEditor(editorPart, false));
+	}
+
+	private IWorkbench getWorkbench() {
+		return PlatformUI.getWorkbench();
+	}
+
+	private IWorkspaceRoot getWorkspaceRoot() {
+		return ResourcesPlugin.getWorkspace().getRoot();
 	}
 
 	private boolean hasFileAsInput(IEditorReference editorRef, IPath path) {
@@ -66,13 +98,23 @@ public class EclipseWorkbenchFixture {
 		return false;
 	}
 
-	private IPath getAbsolutelPath(String filepath) {
-		IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(filepath, false);
+	private IPath getAbsolutePath(String filepath) {
+		IFile file = getFile(filepath);
+		return file.getLocation();
+	}
+
+	private IFile getFile(String filepath) {
+		IResource resource = getWorkspaceRoot().findMember(filepath);
 		if (resource == null) {
 			String message = String.format("Resource with path='%s' does not exist in workspace.", filepath);
 			throw new IllegalArgumentException(message);
 		}
-		return resource.getLocation();
+		if (resource instanceof IFile) {
+			return (IFile) resource;
+		} else {
+			String message = String.format("Resource with path='%s' exists but is not a file.", filepath);
+			throw new IllegalArgumentException(message);
+		}
 	}
 
 	/**
@@ -99,6 +141,33 @@ public class EclipseWorkbenchFixture {
 		}).collect(Collectors.joining(", "));
 		String message = String.format("Could not find editor with filepath='%s', but found editors with inputs: %s", filepath, editorInputs);
 		return new IllegalArgumentException(message);
+	}
+
+	/**
+	 * @return the active {@link IWorkbenchPage} or if there is no active page,
+	 *         the active page of the first workbench window.
+	 */
+	private IWorkbenchPage getActivePageOrFirst() {
+		IWorkbench workbench = getWorkbench();
+		IWorkbenchWindow activeWindow = workbench.getActiveWorkbenchWindow();
+		if (activeWindow != null) {
+			return activeWindow.getActivePage();
+		} else {
+			IWorkbenchWindow firstWindow = workbench.getWorkbenchWindows()[0];
+			return firstWindow.getActivePage();
+		}
+	}
+
+	private <T> T syncExec(Callable<T> callable) {
+		AtomicReference<T> result = new AtomicReference<>();
+		Display.getDefault().syncExec(() -> {
+			try {
+				result.set(callable.call());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
+		return result.get();
 	}
 
 }
