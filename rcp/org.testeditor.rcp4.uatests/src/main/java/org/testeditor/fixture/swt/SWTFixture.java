12b38720c5ca9414cc048b67223ced0d71ae2428
Karsten Panier <kpanier@aysada.org>
2016-09-18 21:44:53 +0200

    TE-423 AML Templates
    * Test for auto completion and templates

--- rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/swt/SWTFixture.java
+++ rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/swt/SWTFixture.java
@@ -406,6 +406,27 @@ public class SWTFixture {
 		activeEditor.toTextEditor().save();
 	}
 
+	@FixtureMethod
+	public void typeTextIntoActiveEditor(String text) {
+		SWTBotEditor activeEditor = bot.activeEditor();
+		logger.info("Type text: {} into editor {}", text, activeEditor.getTitle());
+		activeEditor.toTextEditor().typeText(text);
+	}
+
+	@FixtureMethod
+	public void goToLineInActiveEditor(int lineNumber) {
+		SWTBotEditor activeEditor = bot.activeEditor();
+		logger.info("Go to line: {} into editor {}", lineNumber, activeEditor.getTitle());
+		activeEditor.toTextEditor().navigateTo(lineNumber, 0);
+	}
+
+	@FixtureMethod
+	public void activateAutocompletion(String text, String selectedProposal) {
+		SWTBotEditor activeEditor = bot.activeEditor();
+		logger.info("Activate autocompletion on editor {}", activeEditor.getTitle());
+		activeEditor.toTextEditor().autoCompleteProposal(text, selectedProposal);
+	}
+
 	/**
 	 * Extracts the locator fragment from a locator string type.
 	 * 
