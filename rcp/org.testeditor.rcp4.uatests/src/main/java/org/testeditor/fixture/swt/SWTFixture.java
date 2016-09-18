3b4d5176ad98acc7df4774588eae0c9c8ae5dc83
Karsten Panier <kpanier@aysada.org>
2016-09-18 09:54:21 +0200

    TE-424 Make AML more compact
    * Improve wait for progress dialog

--- rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/swt/SWTFixture.java
+++ rcp/org.testeditor.rcp4.uatests/src/main/java/org/testeditor/fixture/swt/SWTFixture.java
@@ -162,7 +162,7 @@ public class SWTFixture {
 		logger.info("Waiting for dialog with title='{}' to open, timeout='{}' seconds.", title, timeout);
 		try {
 			try {
-				bot.waitUntil(Conditions.shellIsActive(title), 3000);
+				bot.waitUntil(Conditions.shellIsActive(title), timeout * 1000);
 				SWTBotShell shell = bot.shell(title);
 				bot.waitUntil(Conditions.shellCloses(shell), timeout * 1000);
 			} catch (TimeoutException e) {
