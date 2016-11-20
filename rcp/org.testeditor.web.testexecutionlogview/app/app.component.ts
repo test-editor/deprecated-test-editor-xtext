9e2832ac3e1fc32d3a9bf7b0efbafb5ba54a1a5c
Karsten Panier <kpanier@aysada.org>
2016-11-20 22:03:23 +0100

    TE-444 Show test result statistic: Minimal test statistic

--- rcp/org.testeditor.web.testexecutionlogview/app/app.component.ts
+++ rcp/org.testeditor.web.testexecutionlogview/app/app.component.ts
@@ -1,30 +1,36 @@
 import { Component } from '@angular/core';
 import { TestLogService } from './testLog.service';
-import {TestExecutionLog, LogGroup} from './model';
+import {TestExecutionLog, LogGroup, TestRunStatistic} from './model';
 
 @Component({
     selector: 'testexeclog-app',
     templateUrl: 'app/testexeclogapp.html'
 })
 export class AppComponent {
-    testRuns: TestExecutionLog[];
-    testRunLog: String;
-    logGroups: LogGroup[];
-    useLogGroup: boolean;
-    currentSelection: TestExecutionLog;
+    testRuns: TestExecutionLog[]
+    testRunLog: String
+    logGroups: LogGroup[]
+    testExecutionGroup: TestExecutionLog
+    useLogGroup: boolean
+    currentSelection: TestExecutionLog
+    testStatistic:TestRunStatistic
 
     constructor(private logService: TestLogService) {
         this.logService.getTestExecutionLogs().then(testExecLogs => this.testRuns=testExecLogs)
-        this.testRunLog = '';
-        this.useLogGroup = true;
+        this.testRunLog = ''
+        this.useLogGroup = true
     }
 
     onSelect(testExecLog: TestExecutionLog) {
         this.currentSelection = testExecLog
         if(this.useLogGroup) {
-            this.logService.getTestExecutionLogGroups(testExecLog).then(logGroups => this.logGroups=logGroups)
+            this.logService.getTestExecutionLogGroups(testExecLog).then(logGroups => this.testExecutionGroup=logGroups)
+            this.testStatistic = this.testExecutionGroup.testStatistic
+            this.logGroups = this.testExecutionGroup.logGroups
         }else{
             this.logService.getTestExecutionLogContent(testExecLog).then(content => this.testRunLog=content)
+            this.testStatistic = null
+            this.logGroups = null
         }
     }
 
--- rcp/org.testeditor.web.testexecutionlogview/app/model.ts
+++ rcp/org.testeditor.web.testexecutionlogview/app/model.ts
@@ -9,6 +9,7 @@ export class TestExecutionLog {
 
     links: Link[]
     testStatistic: TestRunStatistic
+    logGroups: LogGroup[]
     name: String
     filename: String
     
